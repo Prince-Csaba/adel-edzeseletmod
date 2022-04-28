d648c74d25121aeb03487f6d421694cb834923a9
Prince Csaba <bolyaicsaba@gmail.com>
2022-04-28 10:57:36 +0200

    add some cool js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,34 @@
 function pageLoad () {
-    console.log("Loaded");
+    console.log("Loaded rlly!");
+    let basic = document.getElementById('basic');
+    let full = document.getElementById('full');
+
+    document.getElementById("basicCB").addEventListener("click", showBasic);
+    document.getElementById("fullCB").addEventListener("click", showFull);
+
+    document.getElementById("basicB").addEventListener("click", showBasic);
+    document.getElementById("fullB").addEventListener("click", showFull);
 
     function openNav() {
         console.log("Hiii");
-        var elem = document.getElementById('mobileNav');
+        let elem = document.getElementById('mobileNav');
         elem.classList.toggle("navVisible");
+    }
+
+    function showBasic() {
+        console.log("Basic");
+
+        full.classList.add('d-none');
+        basic.classList.remove('d-none');
+        basic.classList.add('d-flex');
+    }
+
+    function showFull() {
+        console.log("Full");
+
+        basic.classList.add('d-none');
+        full.classList.remove('d-none');
+        full.classList.add('d-flex');
     }
 }
 
