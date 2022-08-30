49d23d7ce016d592beb6a1d76e5211c506c6ccba
Prince Csaba <bolyaicsaba@gmail.com>
2022-08-30 18:24:07 +0200

    *

--- scripts/updateRecipe.js
+++ scripts/updateRecipe.js
@@ -3,7 +3,11 @@ import data from './recipes.json' assert { type: 'json' };
 function pageLoad () {
   console.log("Loaded recipe updater!");
   console.log(data);
-  
+  let recipes = data.recipes;
+  console.log(recipes);
+  for (let recipe of recipes) {
+    console.log(recipe);
+  }
 
 }
 window.addEventListener('load', pageLoad);
