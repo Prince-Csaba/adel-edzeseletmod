d7af6e9b40a6db39dbad276468e42645f68dcc74
Prince Csaba <bolyaicsaba@gmail.com>
2022-10-01 12:32:31 +0200

    add to recipe

--- scripts/updateRecipe.js
+++ scripts/updateRecipe.js
@@ -1,25 +1,27 @@
 import data from "./recipes.json" assert { type: "json" };
 
 function pageLoad() {
-  console.log("Loaded recipe updater!");
-  console.log(data);
+//  console.log("Loaded recipe updater!");
+//  console.log(data);
   let recipes = data.recipes;
-  console.log(recipes);
+//  console.log(recipes);
   let rBody = document.getElementById("recipeBody");
-  console.log(rBody);
+//  console.log(rBody);
   for (let recipe of recipes) {
     let ing = recipe.ingredients;
-    console.log(ing);
+//    console.log(ing);
     let recArr = ing.split(",");
-    console.log(recArr);
-    console.log(recipe.ingredients);
+  //  console.log(recArr);
+  //  console.log(recipe.ingredients);
     let ingredientsList = [];
     for (let i = 0; i < recArr.length; i++) {
-      console.log(i);
+      console.log(recArr[i]);
+      console.log(recArr[i][0]);
+      recArr[i][0] === "$" ? ingredientsList += `<li style="list-style-type:none;font-weight:bold;">${recArr[i].substring(1)}</li>` :
       ingredientsList += `<li>${recArr[i]}</li>`;
     }      
 
-    console.log(ingredientsList);
+//    console.log(ingredientsList);
     let elem = document.createElement("div");
     elem.classList.add("recipeCard");
     elem.innerHTML += `<div class="recImage"><img src="./img/${recipe.img}" alt="${recipe.title}">
@@ -44,18 +46,19 @@ function pageLoad() {
     rBody.appendChild(elem);
   }
 
-  let reclist = document.querySelectorAll("[class=recipeCard]");
-  console.log(reclist);
+/*   let reclist = document.querySelectorAll("[class=recipeCard]");
+  console.log('End function reclist:'+reclist);
   for (let i = 0; i < reclist.length; i++) {
     let element = reclist[i];
     element.addEventListener("click", recVisible);
+    element.setAttribute("onclick", recVisible);
     console.log(element);
   }
 
   function recVisible() {
     console.log("Visible");
     this.classList.toggle("visibleCard");
-  }
+  } */
 }
 window.addEventListener("load", pageLoad);
 
